10795bfec54f4729cb3c24968425d2663d80655a
swlee <swo98@naver.com>
2025-10-05 20:02:15 +0900

    init: yml설정, spock테스트 환경 세팅

---
+++ src/main/java/study/jpa_n_plus_one/JpaNPlusOneApplication.java
@@ -0,0 +1,13 @@
+package study.jpa_n_plus_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNPlusOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaNPlusOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/jpa_n_plus_one/JpaNPlusOneApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpa_n_plus_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNPlusOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
