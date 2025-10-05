faee6d579516a1644cde47e328b23579819c1080
swlee <swo98@naver.com>
2025-10-05 20:45:10 +0900

    add: n+1발생, 엔티티그래프, 배치사이즈, fetchjoin 테스트코드 작성

--- src/main/java/study/jpa_n_plus_one/PostRepository.java
+++ src/main/java/study/jpa_n_plus_one/PostRepository.java
@@ -1,6 +1,22 @@
 package study.jpa_n_plus_one;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    // 베이스 쿼리
+    @Query("select p from Post p")
+    List<Post> findAllPosts();
+
+    // Fetch Join
+    @Query("select distinct p from Post p join fetch p.comments")
+    List<Post> findAllWithFetchJoin();
+
+    // EntityGraph
+    @EntityGraph(attributePaths = {"comments"})
+    List<Post> findAllWithEntityGraph();
 }
--- src/test/groovy/study/jpa_n_plus_one/NPlusOneTest.groovy
+++ src/test/groovy/study/jpa_n_plus_one/NPlusOneTest.groovy
@@ -19,7 +19,7 @@ class NPlusOneTest extends Specification {
     private EntityManager em
 
     def setup() {
-        (1..10).each { i ->
+        (1..5).each { i ->
             def post = new Post("Post " + i)
             postRepository.save(post)
             (1..3).each { j ->
@@ -29,24 +29,58 @@ class NPlusOneTest extends Specification {
         }
         em.flush()
         em.clear()
-        println "--- Test data setup complete ---"
+        println "--- setup() 종료 ---"
     }
 
     @Transactional
-    def "N+1 문제 테스트"() {
+    def "1. Batch Size 테스트 (application.yml 설정)"() {
         given:
-        println "\n\n--- START: N+1 문제 테스트 ---"
+        println "\n\n--- [실행] 1. Batch Size 테스트 ---"
+        println "(주의: application.yml의 default_batch_fetch_size=100 설정이 적용됩니다.)"
 
         when:
-        println "--- 모든 Post 조회 ---"
-        List<Post> posts = postRepository.findAll()
+        println "--- Post 조회 (1번의 쿼리) ---"
+        List<Post> posts = postRepository.findAllPosts()
 
         then:
-        println "--- Post를 순회하며 Comment 지연 로딩 발생 ---"
+        println "--- 각 Post의 Comment 접근 시, IN 절을 사용해 한번에 조회 (1번의 추가 쿼리) ---"
         posts.each { post ->
             println "[로그] Post: ${post.getTitle()}, 댓글 수: ${post.getComments().size()}"
         }
-        println "--- END: N+1 문제 테스트 ---"
-        true
+        println "--- [종료] 1. Batch Size 테스트 ---"
+    }
+
+    @Transactional
+    def "2. Fetch Join 테스트"() {
+        given:
+        println "\n\n--- [실행] 2. Fetch Join 테스트 ---"
+
+        when:
+        println "--- Post와 Comment를 한번에 조회 (1번의 쿼리) ---"
+        List<Post> posts = postRepository.findAllWithFetchJoin()
+
+        then:
+        println "--- 이미 로딩된 Comment 사용 (추가 쿼리 없음) ---"
+        posts.each { post ->
+            println "[로그] Post: ${post.getTitle()}, 댓글 수: ${post.getComments().size()}"
+        }
+        println "--- [종료] 2. Fetch Join 테스트 ---"
+    }
+
+    @Transactional
+    def "3. EntityGraph 테스트"() {
+        given:
+        println "\n\n--- [실행] 3. EntityGraph 테스트 ---"
+
+        when:
+        println "--- Post와 Comment를 한번에 조회 (1번의 쿼리) ---"
+        List<Post> posts = postRepository.findAllWithEntityGraph()
+
+        then:
+        println "--- 이미 로딩된 Comment 사용 (추가 쿼리 없음) ---"
+        posts.each { post ->
+            println "[로그] Post: ${post.getTitle()}, 댓글 수: ${post.getComments().size()}"
+        }
+        println "--- [종료] 3. EntityGraph 테스트 ---"
     }
 }
